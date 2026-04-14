065b4f165dcb2f88a253d1891a3b6aa3b8f5092c
vivek2034 <gamervivek3004@gmail.com>
2026-04-14 12:31:18 +0530

    Create utils.ts

---
+++ src/lib/utils.ts
@@ -0,0 +1,41 @@
+export function formatDate(dateString: string): string {
+  const date = new Date(dateString)
+  return date.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  })
+}
+
+export function calculateReadTime(content: string[]): number {
+  const totalWords = content.join(' ').split(' ').length
+  const wordsPerMinute = 200
+  return Math.ceil(totalWords / wordsPerMinute)
+}
+
+export function debounce<T extends (...args: any[]) => any>(
+  func: T,
+  wait: number
+): (...args: Parameters<T>) => void {
+  let timeout: NodeJS.Timeout
+  
+  return (...args: Parameters<T>) => {
+    clearTimeout(timeout)
+    timeout = setTimeout(() => func(...args), wait)
+  }
+}
+
+export function throttle<T extends (...args: any[]) => any>(
+  func: T,
+  limit: number
+): (...args: Parameters<T>) => void {
+  let inThrottle: boolean
+  
+  return (...args: Parameters<T>) => {
+    if (!inThrottle) {
+      func(...args)
+      inThrottle = true
+      setTimeout(() => inThrottle = false, limit)
+    }
+  }
+}
